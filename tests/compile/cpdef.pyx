6c01078922830dbd50efeac385f043798e540ec3
W. Trevor King <wking@drexel.edu>
2011-02-18 14:34:28 -0500

    Flesh out tests/compile/cpdef.pyx to test cdef-ed enums, structs, and unions.

--- tests/compile/cpdef.pyx
+++ tests/compile/cpdef.pyx
@@ -1,6 +1,25 @@
 cdef class A:
+    cdef public int i
+    cdef readonly int j
+    cdef void *ptr
+
     cpdef a(self):
         ma(self)
 
 cpdef ma(x):
     print x
+
+cdef struct S:
+    cdef public int i
+    cdef readonly int j
+    cdef void *ptr
+
+cdef union S:
+    cdef public int i
+    cdef readonly unsigned int j
+    cdef void *ptr
+
+cdef enum E:
+    cdef public i
+    cdef readonly j
+    k
