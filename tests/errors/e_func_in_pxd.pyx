14435bc0a3edc82dddaad0b82eb92c1d3e27afd0
W. Trevor King <wking@drexel.edu>
2011-02-19 21:02:37 -0500

    Adjust expected test error messages to match p_c_python_binding parsing.

--- tests/errors/e_func_in_pxd.pyx
+++ tests/errors/e_func_in_pxd.pyx
@@ -2,6 +2,6 @@ cimport e_func_in_pxd_support
 
 _ERRORS = u"""
 1:5: function definition in pxd file must be declared 'cdef inline'
-4:5: inline function definition in pxd file cannot be 'public'
+4:12: inline function definition in pxd file cannot be 'public'
 7:5: inline function definition in pxd file cannot be 'api'
 """
--- tests/errors/e_undefexttype.pyx
+++ tests/errors/e_undefexttype.pyx
@@ -2,5 +2,5 @@ cdef class Spam
 cdef extern class external.Eggs
 _ERRORS = u"""
 1:5: C class 'Spam' is declared but not defined
-2:5: C class 'Eggs' is declared but not defined
+2:12: C class 'Eggs' is declared but not defined
 """
